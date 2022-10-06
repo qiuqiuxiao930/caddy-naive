fa915840cb7067f48d9c3ab70f4a4438b087c9da
qiuqiuxiao930 <82669809+qiuqiuxiao930@users.noreply.github.com>
2022-10-06 21:51:59 +0800

    Update caddy-naive.sh

--- caddy-naive.sh
+++ caddy-naive.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt update
 curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+docker stop ubuntu-test
+dcoker rm ubuntu-test
 docker pull qiuqiuxiao930/docker-test:v1
 docker run -p 443:443 -p 80:80 -itd --name ubuntu-test qiuqiuxiao930/docker-test:v1
 docker exec -it ubuntu-test /bin/bash
