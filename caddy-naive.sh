f8711f7bc4ff0c92abb3984ac3b0bcb1b002ca83
qiuqiuxiao930 <82669809+qiuqiuxiao930@users.noreply.github.com>
2022-10-06 21:05:21 +0800

    Create caddy-naive.sh

---
+++ caddy-naive.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt update
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
+docker pull qiuqiuxiao930/docker-test:v1
+docker run -p 443:443 -p 80:80 -itd --name ubuntu-test qiuqiuxiao930/docker-test:v1
+docker exec -it ubuntu-test /bin/bash
+cd
+python3 naive.py
